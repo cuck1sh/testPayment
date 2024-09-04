c6459ea3d74359f01d5f6c83caf8994741bdd5ac
Egor Kirin <egor.kirin20@gmail.com>
2024-09-04 16:46:17 +0600

    initial

---
+++ src/main/java/com/example/testpayment/TestPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testpayment/TestPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
